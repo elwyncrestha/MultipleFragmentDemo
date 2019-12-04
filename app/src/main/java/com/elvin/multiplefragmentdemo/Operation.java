25643842a3c1a6860f8c3da67850c60e5b56bb54
Elvin Shrestha <elwyncrestha@gmail.com>
2019-12-04 23:51:50 +0545

    Add Area of Circle Fragment

---
+++ app/src/main/java/com/elvin/multiplefragmentdemo/Operation.java
@@ -0,0 +1,12 @@
+package com.elvin.multiplefragmentdemo;
+
+/**
+ * @author Elvin Shrestha on 04/12/19
+ */
+public class Operation {
+
+    public static float areaOfCircle(float radius) {
+        return (float) Math.PI * radius * radius;
+    }
+
+}
--- app/src/main/java/com/elvin/multiplefragmentdemo/fragment/CircleFragment.java
+++ app/src/main/java/com/elvin/multiplefragmentdemo/fragment/CircleFragment.java
@@ -2,19 +2,25 @@ package com.elvin.multiplefragmentdemo.fragment;
 
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.fragment.app.Fragment;
+
+import com.elvin.multiplefragmentdemo.Operation;
 import com.elvin.multiplefragmentdemo.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class CircleFragment extends Fragment {
+public class CircleFragment extends Fragment implements View.OnClickListener {
+
+    private EditText etRadius;
+    private Button btnCalculateArea;
 
 
     public CircleFragment() {
@@ -26,7 +32,15 @@ public class CircleFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_circle, container, false);
+        View view = inflater.inflate(R.layout.fragment_circle, container, false);
+        this.etRadius = view.findViewById(R.id.etRadius);
+        this.btnCalculateArea = view.findViewById(R.id.btnCalculateArea);
+        this.btnCalculateArea.setOnClickListener(this);
+        return view;
     }
 
+    @Override
+    public void onClick(View v) {
+        Toast.makeText(getActivity(), "Area of circle is: " + Operation.areaOfCircle(Float.parseFloat(this.etRadius.getText().toString())), Toast.LENGTH_SHORT).show();
+    }
 }
