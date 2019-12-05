7a35bf90108837f0537519384ca5fe3022f6eb81
Elvin Shrestha <elwyncrestha@gmail.com>
2019-12-05 08:15:03 +0545

    Add View Pager and Tab Layout

--- app/src/main/java/com/elvin/multiplefragmentdemo/activity/ViewPagerActivity.java
+++ app/src/main/java/com/elvin/multiplefragmentdemo/activity/ViewPagerActivity.java
@@ -8,7 +8,11 @@ import android.os.Bundle;
 
 import com.elvin.multiplefragmentdemo.R;
 import com.elvin.multiplefragmentdemo.adapter.ViewPagerAdapter;
+import com.elvin.multiplefragmentdemo.fragment.AutomorphicFragment;
 import com.elvin.multiplefragmentdemo.fragment.CircleFragment;
+import com.elvin.multiplefragmentdemo.fragment.PalindromeFragment;
+import com.elvin.multiplefragmentdemo.fragment.ReverseNumberFragment;
+import com.elvin.multiplefragmentdemo.fragment.ReverseStringFragment;
 import com.elvin.multiplefragmentdemo.fragment.SumFragment;
 import com.google.android.material.tabs.TabLayout;
 
@@ -29,6 +33,10 @@ public class ViewPagerActivity extends AppCompatActivity {
         ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPagerAdapter.addFragment("Sum", new SumFragment());
         viewPagerAdapter.addFragment("Area of circle", new CircleFragment());
+        viewPagerAdapter.addFragment("Palindrome", new PalindromeFragment());
+        viewPagerAdapter.addFragment("Automorphic", new AutomorphicFragment());
+        viewPagerAdapter.addFragment("Reverse String", new ReverseStringFragment());
+        viewPagerAdapter.addFragment("Reverse Number", new ReverseNumberFragment());
 
         viewPager.setAdapter(viewPagerAdapter);
         tabLayout.setupWithViewPager(viewPager);
